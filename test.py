5d2f31a4efb7bda3c96fba3550ebb7b4535681ff
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-04-17 22:53:09 +0300

    Added lab_5

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+from lab_5 import bfs
+
+class TestBFS(unittest.TestCase):
+
+    def test_start_equals_end(self):
+        dist, _ = bfs(8, (4, 4), (4, 4))
+        self.assertEqual(dist, 0)
+
+    def test_direct_move(self):
+        dist, _ = bfs(8, (0, 0), (2, 1))
+        self.assertEqual(dist, 1)
+
+    def test_reachable_far(self):
+        dist, _ = bfs(8, (0, 0), (7, 7))
+        self.assertEqual(dist, 6)
+
+    def test_unreachable_small_board(self):
+        dist, _ = bfs(2, (0, 0), (1, 1))
+        self.assertEqual(dist, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
