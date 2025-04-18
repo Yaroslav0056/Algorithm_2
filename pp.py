d515b98c477804acb4ab24644f7caefe5774961e
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-04-18 11:40:06 +0300

    Fixed

--- pp.py
+++ pp.py
@@ -8,6 +8,7 @@ def is_in(x, y, N, visited):
 
 def bfs(N, start, end):
     visited = [[False] * N for _ in range(N)]
+    parent = [[None] * N for _ in range(N)]
 
     q = deque()
     start_x, start_y = start
@@ -19,7 +20,7 @@ def bfs(N, start, end):
         x, y, dist = q.popleft()
 
         if (x, y) == (end_x, end_y):
-            return dist
+            return dist, parent
 
         for move_x, move_y in zip(row_moves, col_moves):
             new_x = x + move_x
@@ -27,9 +28,20 @@ def bfs(N, start, end):
 
             if is_in(new_x, new_y, N, visited):
                 visited[new_x][new_y] = True
+                parent[new_x][new_y] = (x, y)
                 q.append((new_x, new_y, dist + 1))
 
-    return -1
+    return -1, parent
+
+def reconstruct_path(parent, start, end):
+    path = []
+    current = end
+    while current != start:
+        path.append(current)
+        current = parent[current[0]][current[1]]
+    path.append(start)
+    path.reverse()
+    return path
 
 def main():
     with open("input.txt", "r") as f:
@@ -37,10 +49,21 @@ def main():
         start_x, start_y = map(int, f.readline().strip().split(","))
         end_x, end_y = map(int, f.readline().strip().split(","))
 
-    min_steps = bfs(N, (start_x, start_y), (end_x, end_y))
+    min_steps, parent = bfs(N, (start_x, start_y), (end_x, end_y))
 
     with open("output.txt", "w", encoding="UTF-8") as f:
         f.write(f"Мінімальна кількість кроків: {min_steps}\n")
+
+    if min_steps != -1:
+        path = reconstruct_path(parent, (start_x, start_y), (end_x, end_y))
+        with open("output.txt", "a", encoding="UTF-8") as f:
+            f.write("Шлях: \n")
+            for step in path:
+                f.write(f"({step[0]}, {step[1]})\n")
+    else:
+        print("Шлях не знайдено")
+        with open("output.txt", "a", encoding="UTF-8") as f:
+            f.write("Шлях не знайдено\n")
 
 if __name__ == "__main__":
     main()
