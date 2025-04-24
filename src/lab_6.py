a6de27a76196e4fbc0078815fbfd84bb228db73f
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-04-24 20:31:17 +0300

    Modified lab_6

--- src/lab_6.py
+++ src/lab_6.py
@@ -1,23 +1,4 @@
 def minimum_beers(N, B, raw):
-    try:
-        if not (str(N).isdigit() and str(B).isdigit()):
-            raise ValueError("N і B повинні бути цілими додатними числами")
-
-        N, B = int(N), int(B)
-
-        if N <= 0 or B <= 0:
-            raise ValueError("Кількість людей і сортів пива повинна бути більшою за 0")
-
-        if len(raw) != N * B:
-            raise ValueError(f"Невірна довжина рядка raw")
-
-        if any(c not in 'YN' for c in raw):
-            raise ValueError("Рядок raw може містити лише символи Y або N")
-
-    except ValueError as e:
-        print(f"Помилка: {e}")
-        return None
-
     likes = [set() for _ in range(B)]
 
     for i in range(N):
@@ -46,7 +27,25 @@ def minimum_beers(N, B, raw):
 
     return len(selected_beers)
 
+def main():
+    try:
+        N_B_input = input("Введіть кількість людей та кількість сортів пива через пробіл: ").split()
+        if len(N_B_input) != 2 or not all(s.isdigit() for s in N_B_input):
+            raise ValueError("Потрібно ввести два цілих додатних числа через пробіл")
+        N, B = map(int, N_B_input)
+
+        raw = input("Введіть переваги: ").replace(" ", "").upper()
+        if any(c not in 'YN' for c in raw):
+            raise ValueError("Переваги мають містити лише символи Y або N")
+        if len(raw) != N * B:
+            raise ValueError(f"Невірно введені переваги")
+
+        result = minimum_beers(N, B, raw)
+        if result is not None:
+            print(result)
+
+    except ValueError as e:
+        print(f"Помилка: {e}")
+
 if __name__ == '__main__':
-    N, B = map(int, input().split())
-    raw = input().replace(" ", "")
-    print(minimum_beers(N, B, raw))
+   main()
