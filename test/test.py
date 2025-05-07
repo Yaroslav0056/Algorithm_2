f2de2d24013ac9d7d7c9b016fc23e3766da153ad
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-05-08 01:17:19 +0300

    Modified test.py

--- test/test.py
+++ test/test.py
@@ -5,10 +5,10 @@ from src.lab_7 import kmp_search
 class TestKMPSearch(unittest.TestCase):
 
     def test_multiple_occurrences(self):
-        self.assertEqual(kmp_search("лілі", "лілілось лілілось"), [(0, 3), (10, 13)])
+        self.assertEqual(kmp_search("лілі", "лілілось лілілось"), [(0, 3), (9, 12)])
 
     def test_single_occurrence(self):
-        self.assertEqual(kmp_search("ліліло", "лілілось лілілось"), [(0, 5)])
+        self.assertEqual(kmp_search("ліліло", "лілілось лілілось"), [(0, 5), (9, 14)])
 
     def test_no_match(self):
         self.assertEqual(
@@ -16,7 +16,7 @@ class TestKMPSearch(unittest.TestCase):
         )
 
     def test_partial_match_only(self):
-        self.assertEqual(kmp_search("лох", "лілілохсь лілілохсь"), [(4, 6), (15, 17)])
+        self.assertEqual(kmp_search("лох", "лілілохсь лілілохсь"), [(4, 6), (14, 16)])
 
     def test_empty_needle(self):
         self.assertEqual(kmp_search("", "щось тут є"), [])
