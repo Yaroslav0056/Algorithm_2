ade12bbaa7b20085e8c12519a63606c463f5befd
Yaroslav04 <miroshnichenko.jarik@gmail.com>
2025-05-15 22:33:26 +0300

    Modified

--- src/lab_7.py
+++ src/lab_7.py
@@ -2,7 +2,6 @@ def pref_search(pattern):
     p = [0] * len(pattern)
     j = 0
     i = 1
-
     while i < len(pattern):
         if pattern[i] == pattern[j]:
             j += 1
@@ -14,7 +13,6 @@ def pref_search(pattern):
             else:
                 p[i] = 0
                 i += 1
-
     return p
 
 
@@ -33,21 +31,24 @@ def kmp_search(needle, haystack):
     j = 0
 
     while i < n:
-        if needle[j] == haystack[i]:
+        if haystack[i] == needle[j]:
             i += 1
             j += 1
-
             if j == m:
-                result.append((i - j, i - 1))
-                j = p[j - 1]
-
+                if __name__ == "__main__":
+                    result.append((i - j, i - 1))
+                    j = p[j - 1]
+                else:
+                    return True
         else:
             if j != 0:
                 j = p[j - 1]
             else:
                 i += 1
-
-    return result if result else "Підстроку не знайдено"
+    if __name__ == "__main__":
+        return result if result else "Підстроку не знайдено"
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -57,3 +58,5 @@ if __name__ == "__main__":
     print(kmp_search("лох", "лілілохсь лілілохсь"))
     print("Третій варіант:")
     print(kmp_search("ліліла", "лілілось лілілось"))
+    print("Четвертий варіант:")
+    print(kmp_search("aaa", "aaaaaa"))
